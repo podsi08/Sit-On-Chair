127f44cff74f01dc98fcd9b96781a45c867bbe23
Ola Tobiszewska <aleksandra.tobiszewska08@gmail.com>
2018-05-10 15:49:18 +0200

    RWD beginning with Sass

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var sass = require('gulp-sass');
+var sourcemaps = require("gulp-sourcemaps");
+
+gulp.task('sass', function() {
+return gulp.src('sass/**/*.scss')
+.pipe(sourcemaps.init())
+.pipe(sass(
+    {errLogToConsole: true,
+        outputStyle: "expanded"
+        }))
+.pipe(sourcemaps.write())
+.pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
